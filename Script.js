b2a7ea1d99b8227a1d6fa2d60ad4d42d7d6cf9fe
Sangam Devkots <sangamdevkota02@gmail.com>
2024-01-23 09:55:13 +0545

    email fetcbed

--- Script.js
+++ Script.js
@@ -1,33 +1,36 @@
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
-    // Sample book data (you can replace this with a fetch call to your server or an API)
+
     const booksData = [
         {
             image: "https://images-na.ssl-images-amazon.com/images/I/718ReYbwlFL.jpg",
             name: "After You",
             author: "by Jojo Myoes",
             rating: "1.987 rating",
-            info: "It continues the story of Louisa Clark after Will's death. She is trying to move on."
+            info: "Book Posted by Sangam Devkota"
         },
         {
             image: "https://images-na.ssl-images-amazon.com/images/I/91JxVjINNsL.jpg",
             name: "Big Magic",
             author: "by Elizabeth Gilbert",
             rating: "1.987 rating",
-            info: "Readers of all ages and walks of life have drawn inspiration from Elizabeth Gilbert’s books."
+            info: "Book Posted by Sangam Devkota"
         },
         {
             image: "https://images-na.ssl-images-amazon.com/images/I/9129dzchsGL.jpg",
             name: "A Tale for the Time Being",
             author: "by Ruth Ozeki",
             rating: "1.987 rating",
-            info: "In Tokyo, sixteen-year-old Nao has decided there’s only one escape from her aching loneliness."
+            info: "Book Posted by Sangam Devkota"
         },
         {
             image: "https://images-na.ssl-images-amazon.com/images/I/81djg0KWthS.jpg",
             name: "The Great Gatsby",
             author: "by F. Scott Fitzgerald",
             rating: "1.987 rating",
-            info: "The novel was inspired by a youthful romance Fitzgerald had with socialite Ginevra King."
+            info: "Book Posted by Sangam Devkota"
         }
     ];
 
---
+++ process-form.php
@@ -0,0 +1,40 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $bookName = htmlspecialchars($_POST["book_name"]);
+    $author = htmlspecialchars($_POST["author"]);
+    $bookCondition = htmlspecialchars($_POST["book_condition"]);
+    $reasonToExchange = htmlspecialchars($_POST["reason_to_exchange"]);
+    $firstName = htmlspecialchars($_POST["first_name"]);
+    $lastName = htmlspecialchars($_POST["last_name"]);
+    $phone = htmlspecialchars($_POST["phone"]);
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+
+
+    $to = "sangamdevkota02@gmail.com";  
+    $subject = "New Book Exchange Request";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+
+    $emailMessage = "Book Name: $bookName\n";
+    $emailMessage .= "Author: $author\n";
+    $emailMessage .= "Book Condition: $bookCondition\n";
+    $emailMessage .= "Reason to Exchange: $reasonToExchange\n";
+    $emailMessage .= "First Name: $firstName\n";
+    $emailMessage .= "Last Name: $lastName\n";
+    $emailMessage .= "Phone: $phone\n";
+    $emailMessage .= "Email: $email\n";
+
+  
+    if (mail($to, $subject, $emailMessage, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Error sending email. Please check your server configuration.";
+    }
+} else {
+ 
+    header("Location: Exchange.html");
+}
+?>
